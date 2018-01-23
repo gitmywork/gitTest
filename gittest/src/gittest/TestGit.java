f4bbd1f57b6ab387da4a07e7bdc7866ed7a8ace6
gitmywork <35282244+gitmywork@users.noreply.github.com>
2018-01-23 14:30:21 +0800

    Update TestGit.java

--- gittest/src/gittest/TestGit.java
+++ gittest/src/gittest/TestGit.java
@@ -3,7 +3,7 @@ package gittest;
 public class TestI {
 	public static void main(String[] args) {
 		int sum=0;
-		for（int i=0,i++,i<10）{
+		for（int i=0；i<10；i++）{
 			sum+=i;
 		}
 		system.out.print("sum="+sum);
