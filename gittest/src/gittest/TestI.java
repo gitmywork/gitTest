dbdf1119e43223ee51ffa685c2cbe403592d9676
gitmywork <793986885@qq.com>
2018-01-23 15:31:13 +0800

    第二次提交

--- gittest/src/gittest/TestI.java
+++ gittest/src/gittest/TestI.java
@@ -3,10 +3,10 @@ package gittest;
 public class TestI {
 	public static void main(String[] args) {
 		int sum=0;
-		for（int i=0,i++,i<10）{
+		for (int i = 0; i < 10; i++) {
 			sum+=i;
 		}
-		system.out.print("sum="+sum);
+		System.out.println("sum="+sum);
 	}
 
 }
